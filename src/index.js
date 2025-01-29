c588174b9c34be10d2ed9e5be980fc0039c0c55c
Diogo Valongo <64019570+diogo-valongo@users.noreply.github.com>
2025-01-29 13:47:50 -0300

    feat: criando interface de acesso a documentação da API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,91 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const cors = require('cors'); // Importa o pacote cors
 const app = express();
 const port = 3000;
 
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Calculadora',
+            version: '1.0.0',
+            description: 'API para realizar operações matemáticas básicas',
+        },
+    },
+    apis: ['./src/index.js'], // Caminho para os arquivos onde estão as anotações
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+// Defina a rota para a documentação
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(cors()); // Adiciona o middleware CORS
 app.use(express.json());
 
-// Rota para somar dois números
+/**
+ * @swagger
+ * /soma:
+ *   post:
+ *     summary: Soma dois números
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               num1:
+ *                 type: number
+ *               num2:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Resultado da soma
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 resultado:
+ *                   type: number
+ */
 app.post('/soma', (req, res) => {
     const { num1, num2 } = req.body;
     const resultado = num1 + num2;
     res.json({ resultado });
 });
 
-// Rota para subtrair dois números
+/**
+ * @swagger
+ * /subtracao:
+ *   post:
+ *     summary: Subtrai dois números
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               num1:
+ *                 type: number
+ *               num2:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Resultado da subtração
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 resultado:
+ *                   type: number
+ */
 app.post('/subtracao', (req, res) => {
     const { num1, num2 } = req.body;
     const resultado = num1 - num2;
